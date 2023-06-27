0d578627b0e2b2db15818b304b821ad5b9094cec
SevakYapujyan <135138666+SevakYapujyan@users.noreply.github.com>
2023-06-27 12:58:11 +0400

    Add files via upload

---
+++ enter_password.py
@@ -0,0 +1,11 @@
+
+
+
+password = "0000"
+user_password = input("enter your password -->")
+
+def enter_password (user_password):
+    return "password matches" if user_password == password else "no matches"
+
+print (enter_password(user_password))
+
